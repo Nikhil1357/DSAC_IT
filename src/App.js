3f13e35d8736bbcf6221bc0d7e5014da51512beb
Nikhil1357 <lakshmim1357@gmail.com>
2023-04-27 22:55:33 +0530

    Sync of Auth and Frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './screens/main';
-import Vote from './screens/secpage';
+import AuthProvider from './contexts/AuthContext';
+import { BrowserRouter, Router, Route, Routes, Switch, Navigate } from 'react-router-dom';
+import RequireAuth from './PrivateRoute/RequireAuth';
+import Vote from './screens/Vote';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Main/> */}
-      {/* <Vote/> */}
-    </div>
+   return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/Vote' element={
+              <RequireAuth>
+                <Vote/>
+              </RequireAuth>
+          }/>
+          <Route exact path="/" element={<Main/>}/>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+// import { initializeApp } from "firebase/app";
+// import * as firebase from "./firebase";
+// const app = initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// })
+
+// console.log(firebase);
+
+// // console.log(typeof(firebase.default.auth));
+
+// // export const auth = app.auth();
+// export default app;
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { getFirestore } from 'firebase/firestore';
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+})
+
+
+export const auth = app.auth();
+export const db = getFirestore(app);
+export default app;
